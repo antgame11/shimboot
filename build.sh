239371e3b33fd5a757a2040439fee275713386af
ading2210 <ading2019@gmail.com>
2026-02-15 01:07:04 -0800

    remove 7z dependency. i dont know why this was ever added in the first place.

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ print_help() {
 }
 
 assert_root
-assert_deps "cpio binwalk pcregrep realpath cgpt mkfs.ext4 mkfs.ext2 fdisk lz4 7z"
+assert_deps "cpio binwalk pcregrep realpath cgpt mkfs.ext4 mkfs.ext2 fdisk lz4"
 assert_args "$3"
 parse_args "$@"
 
